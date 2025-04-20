56d6614d8bf5225220e1f0e28af5aed5006fce66
DayanaraBV <dayanara.bone@uees.edu.ec>
2025-04-19 22:14:51 -0500

    Consumiendo la FakeStore API para visualizar productos

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,24 @@
+import { useState, useEffect } from "react"
 import Layout from "../Components/Layout"
 import Card from "../Components/Card"
 function Home() {
+  const [items, setItems] = useState(null)
+  useEffect(()=>{
+    fetch("https://api.escuelajs.co/api/v1/products")
+    .then(response => response.json())
+    .then(data => setItems(data))
+  }, [])
     return (
         <Layout>
           Home
-          <Card />
+          <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+           {
+              items?.map((items)=>{
+              return <Card  key={items.id} data={items}/>
+              })
+           }
+          </div>
         </Layout>
-  
     )
   }
   
